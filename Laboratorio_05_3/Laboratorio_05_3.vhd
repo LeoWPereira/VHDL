9a556e96e1d58b3c28ce835cde6c7a3ba46a651c
LeoWPereira <leonardowinterpereira@gmail.com>
2018-04-11 16:15:29 -0300

    Fix Lab05_03

--- Laboratorio_05_3/Laboratorio_05_3.vhd
+++ Laboratorio_05_3/Laboratorio_05_3.vhd
@@ -1,36 +1,49 @@
+-------------------------------------
+
+-- Crie um detector de padrões simples, utilizando M chaves para o padrão a ser procurado e N chaves para o vetor	de	busca.	
+-- A saída do circuito deve indicar se o padrão foi encontrado no vetor ou não. Por exemplo, com M = 3 e se o padrão for “101” e	
+-- o vetor de busca “1101000” (N = 7), o padrão foi encontrado e o saida deve estar acesso.
+
+-------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
---
+
+-------------------------------------
+
 entity Laboratorio_05_3 is
 	generic (
-		M:NATURAL := 3;
-		N:NATURAL := 7
+		N : natural := 7;
+		
+		M : natural := 3
 	);
 	
 	port (
-		MASK: IN STD_LOGIC_VECTOR (M-1 DOWNTO 0);
-		BUSCA: IN STD_LOGIC_VECTOR (N-1 DOWNTO 0);
-		LED: OUT STD_LOGIC
+		mascara			: in std_logic_vector ((M - 1) downto 0);
+		
+		vetorEntrada	: in std_logic_vector ((N - 1) downto 0);
+		
+		saida				: out std_logic
 	);
 end entity;
---
-architecture Laboratorio_05_3 of Laboratorio_05_3 IS
-	TYPE DETECTOR IS ARRAY (0 TO N-M) OF STD_LOGIC;
-	SIGNAL detect: DETECTOR;
-begin
+
+-------------------------------------
+
+architecture Laboratorio_05_3 of Laboratorio_05_3 is
+	type COMPARADOR is array (0 to (N - M)) of std_logic;
 	
-	detect(0) <= '1' when (BUSCA(M-1 DOWNTO 0) AND MASK) = MASK else
-					 '0';
-					 
-	BUSCA_GEN: FOR i IN 1 TO N-M GENERATE
-		
-		detect(i) <= detect(i-1) or '1' when (BUSCA(M-1+i DOWNTO i) AND MASK) = MASK else
-						 detect(i-1) or '0';
+	signal procuraPadrao: COMPARADOR;
+	
+	begin
+		procuraPadrao(0) <= '1' when ((vetorEntrada((M - 1) downto 0) and mascara) = mascara) else
+						 '0';
 						 
-	END GENERATE BUSCA_GEN;
-	
-	LED <= detect(N-M);
-	
+		BUSCA: for i in 1 to N-M generate
+			procuraPadrao(i) <= procuraPadrao(i - 1) or '1' when ((vetorEntrada((M - 1 + i) downto i) and mascara) = mascara) else
+									  procuraPadrao(i - 1) or '0';
+		END GENERATE BUSCA;
+		
+		saida <= procuraPadrao(N - M);	
 end architecture;
 
